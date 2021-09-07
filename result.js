ec5012873dde121cf5e18faaacefc7a8e19c79f2
satomi <satomi3813281@gmail.com>
2021-09-07 18:00:59 +0900

    期間を表示できるようにしました

--- result.js
+++ result.js
@@ -4,22 +4,22 @@ function hello(){
     console.log("result.jsの中のhello");
     const snack = document.getElementById("count").value;
     console.log(snack);
-    const mounths = document.getElementById("mounth").value;
-    console.log(mounths);
-    const cal = Number(snack) * 299 * Number(mounths) *30;
+    const months = document.getElementById("month").value;
+    console.log(months);
+    const cal = Number(snack) * 299 * Number(months) *30;
     const kg = Number(cal) / 7200;
     console.log(kg);
 
     if(kg<=5){
         console.log("痩せる");
-        window.location.href = 'resultNoprobrem.html?kg&mounths=' + kg.toString();
+        window.location.href = 'resultNoprobrem.html?kg='+kg.toString()+'&month='+months.toString();
   
     }else if(kg>5 && kg<=10){
         console.log("普通");
-        window.location.href = 'resultLittlefat.html?kg=' + kg.toString();
+        window.location.href = 'resultLittlefat.html?kg='+kg.toString()+'&month='+months.toString();
     }else if(kg>10){
         console.log("デブ");
-        window.location.href = 'resultFat.html?kg=' + kg.toString();
+        window.location.href = 'resultFat.html?kg='+kg.toString()+'&month='+months.toString();
     }
 
 }
